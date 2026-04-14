4e0510b0d7f7827b78e6e0bf74dd9af2ee681d83
rovikko <57286337+rovikko@users.noreply.github.com>
2026-04-14 14:13:16 +0300

    smth

--- dark-academy-test/src/App.tsx
+++ dark-academy-test/src/App.tsx
@@ -1,8 +1,23 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return (<>
-  </>)
+  return (
+    <>
+      <div>header</div>
+      <div className="app-container">
+        <div className="app-part-container">
+          <h1 className="app-title">1</h1>
+        </div>
+        <div className="app-part-container">
+          <h1 className="app-title">2</h1>
+        </div>
+        <div className="app-part-container">
+          <h1 className="app-title">3</h1>
+        </div>
+      </div>
+      <div>footer</div>
+    </>
+  );
 }
 
-export default App
+export default App;
